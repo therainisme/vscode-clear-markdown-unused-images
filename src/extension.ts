f4cc0af87a8645258e63d1434f59e4e2b9e82b7c
therainisme <therainisme@qq.com>
2024-02-29 15:31:07 +0800

    initial publish

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('clear-markdown-unused-images.clear', async () => {
+	let disposable = vscode.commands.registerCommand('clear-markdown-unused-images.run', async () => {
 		// The code you place here will be executed every time your command is executed
 		vscode.window.showInformationMessage('Searching for unused images in markdown files...');
 
